7bd0e69d3ea6f4b8505c4e1ed750d9e45701def3
Earl Oliver <earl.oliver@gmail.com>
2025-03-03 14:10:48 -0500

    Moved lazy and options into sub lua files for cleanliness

--- lua/lazy.lua
+++ lua/lazy.lua
@@ -1,89 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
---require("config.lazy")
-
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
--- Pull in all the vim setup options.
-require 'options'
-
--- Moved all the keymappings into their own file.
-require 'keymaps'
-
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -95,6 +9,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+-- Setup the Lazy package manager
+require 'lazy'
 
 -- [[ Configure and install plugins ]]
 --
@@ -835,6 +752,7 @@ require('lazy').setup({
   -- Highlight, edit, and navigate code
   require 'plugins.treesitter',
 
+  -- Plugin to colourize color codes like #FF00FF
   require 'plugins.colorizer',
 
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
@@ -889,57 +807,3 @@ require('lazy').setup({
     },
   },
 })
-
--- Set the colour scheme
--- vim.cmd.colorscheme 'torte' -- previous mvim theme
--- vim.cmd.colorscheme 'catppuccin-mocha'
--- vim.cmd.colorscheme 'tokyonight-night'
-vim.cmd.colorscheme 'carbonfox'
-vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'none' })
-
-if vim.g.neovide then
-  -- Performance debugging tool
-  vim.g.neovide_profiler = false
-
-  -- Mac keyboards don't have an alt key, so we use the command key.
-  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
-
-  -- toggle fullscreen within neovide
-  vim.keymap.set('n', ';f', function()
-    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
-  end, { desc = 'Toggle [f]ullscreen' })
-
-  -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = 'Inconsolata:h15'
-  vim.g.neovide_opacity = 1
-  vim.g.neovide_normal_opacity = 1
-  vim.g.neovide_window_blurred = false
-
-  -- cursor
-  vim.g.neovide_cursor_antialiasing = true
-  vim.g.neovide_cursor_animation_length = 0.05
-  vim.g.neovide_cursor_animate_in_insert_mode = true
-  vim.g.neovide_cursor_animate_command_line = true
-
-  -- animations
-  vim.g.neovide_position_animation_length = 0.15
-  vim.g.neovide_scroll_animation_length = 0.250
-  vim.g.neovide_scroll_animation_far_lines = 1
-
-  -- hide the mouse when typing
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_theme = 'dark' -- dark | light | auto
-
-  -- cursor particles
-  vim.g.neovide_cursor_vfx_mode = 'railgun'
-
-  -- Animations can be disabled as follows
-  -- vim.g.neovide_position_animation_length = 0
-  -- vim.g.neovide_cursor_animation_length = 0.00
-  -- vim.g.neovide_cursor_trail_size = 0
-  -- vim.g.neovide_scroll_animation_far_lines = 0
-  -- vim.g.neovide_scroll_animation_length = 0.00
-end
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
---
+++ lua/options.lua
@@ -0,0 +1,96 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = false
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 0
+
+--
+-- Settings from my init.vim file
+--
+
+-- backup, swap and undo
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.writebackup = false
+vim.opt.undofile = false
+vim.opt.autoread = true
+
+-- indent
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true -- converts tabs to white space
+vim.opt.shiftwidth = 4 -- width for autoindents
+vim.opt.tabstop = 4 -- number of columns occupied by a tab
+vim.opt.autoindent = true -- indent a new line the same amount as the line just typed
+vim.opt.softtabstop = 2 -- see multiple spaces as tabstops so <BS> does the right thing
+
+-- search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.wrapscan = true
+vim.opt.smartcase = true -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+
+-- sign column
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = 'yes'
+
+-- status and tab bar
+vim.opt.laststatus = 2
+-- vim.opt.laststatus = 3 -- only show a single status line
+vim.opt.showmode = false
+vim.opt.showtabline = 1 -- show tabline if there is more than 1 tab
+vim.opt.termguicolors = true
+
+-- get bash-like tab completions
+vim.opt.wildmode = 'longest,list,full'
+-- show matching
+vim.opt.showmatch = true
+-- set an 80 column border for good coding style
+vim.opt.colorcolumn = '80'
+-- Se the text width to 80 characters.
+vim.opt.textwidth = 80
+vim.opt.wrap = true
+
+-- ignore some files for filename completion
+vim.opt.wildignore = '*.o,*.r,*.so,*.sl,*.tar,*.tgz'

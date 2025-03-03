c9b7d6adac0d212a3e5a21cad72189d66376349a
Earl Oliver <earl.oliver@gmail.com>
2025-03-02 19:31:05 -0500

    Added incremental node selection to treesitter configs

--- init.lua
+++ init.lua
@@ -965,6 +965,8 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
+      -- Autoinstall languages that are not installed
+      auto_install = true,
       ensure_installed = {
         'bash',
         'c',
@@ -978,14 +980,21 @@ require('lazy').setup({
         'vim',
         'vimdoc',
       },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = 'vn',
+          node_incremental = 'vn',
+          scope_incremental = false,
+          node_decremental = '<BS>',
+        },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },

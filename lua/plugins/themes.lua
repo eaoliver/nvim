71d0ec7f1af128f72e7b2128a40083065570c6a9
Earl Oliver <earl.oliver@gmail.com>
2025-03-06 12:05:09 -0500

    Added arctic theme and set as default

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -63,4 +63,14 @@ return {
       }
     end,
   },
+  {
+    'rockyzhang24/arctic.nvim',
+    dependencies = { 'rktjmp/lush.nvim' },
+    name = 'arctic',
+    branch = 'main',
+    priority = 1000,
+    config = function()
+      -- vim.cmd("colorscheme arctic")
+    end,
+  },
 }

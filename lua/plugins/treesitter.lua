8dc9cc97bd4774becd971ebd4a040f6ceec1976b
Earl Oliver <earl.oliver@gmail.com>
2025-04-06 18:31:09 -0400

    Enabled auto-closing of tags

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,25 @@ return {
     build = ':TSUpdate',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
-      'windwp/nvim-ts-autotag',
+      {
+        'windwp/nvim-ts-autotag',
+        opts = {
+          opts = {
+            -- Defaults
+            enable_close = true, -- Auto close tags
+            enable_rename = true, -- Auto rename pairs of tags
+            enable_close_on_slash = false, -- Auto close on trailing </
+          },
+          -- Also override individual filetype configs, these take priority.
+          -- Empty by default, useful if one of the "opts" global settings
+          -- doesn't work well in a specific filetype
+          -- per_filetype = {
+          --   ['html'] = {
+          --     enable_close = false,
+          --   },
+          -- },
+        },
+      },
     },
     event = { 'BufWritePre', 'BufNewFile' },
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts

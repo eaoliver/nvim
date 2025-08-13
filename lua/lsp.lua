436baa2d4489aa2e1ec5c50995bfde133aa34cc1
Earl Oliver <earl.oliver@gmail.com>
2025-08-13 13:43:16 -0400

    Change LSP Keybindings to Match the Default gr Bindings Introduced in Neovim 0.11

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -80,41 +80,39 @@ return {
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[g]oto [d]efinition')
+          map('grd', require('telescope.builtin').lsp_definitions, '[gr]oto [d]efinition')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map('gD', vim.lsp.buf.declaration, '[g]oto [D]eclaration')
+          map('grD', vim.lsp.buf.declaration, '[gr]oto [D]eclaration')
+
+          -- Rename the variable under your cursor.
+          --  Most Language Servers support renaming across files, etc.
+          map('grn', vim.lsp.buf.rename, '[gr]oto [r]e[n]ame')
+
+          -- Execute a code action, usually your cursor needs to be on top of an error
+          -- or a suggestion from your LSP for this to activate.
+          map('gra', vim.lsp.buf.code_action, '[gr]oto Code [a]ction', { 'n', 'x' })
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[g]oto [r]eferences')
+          map('grr', require('telescope.builtin').lsp_references, '[gr]oto [r]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gi', require('telescope.builtin').lsp_implementations, '[g]oto [i]mplementation')
+          map('gri', require('telescope.builtin').lsp_implementations, '[gr]oto [i]mplementation')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('gt', require('telescope.builtin').lsp_type_definitions, '[g] [t]ype definition')
+          map('grt', require('telescope.builtin').lsp_type_definitions, '[gr]oto [t]ype definition')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[d]ocument [s]ymbols')
+          map('gO', require('telescope.builtin').lsp_document_symbols, '[g]oto [O]pen document symbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[w]orkspace [s]ymbols')
-
-          -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
-          -- This has been replaced with grn in nvim-0.11
-          -- map('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
-
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
-          -- Replaced with gra in nvim-0.11
-          -- map('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction', { 'n', 'x' })
+          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[g]oto [w]orkspace symbols')
 
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
